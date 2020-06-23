ed64d32442b73b76804cc60b2727d89413fea1bf
zoeseveriens <zoe.222@hotmail.com>
2020-06-23 09:16:11 +0200

    worked on PE

--- script.js
+++ script.js
@@ -11,20 +11,16 @@ const firstField = document.getElementById('first');
 const secondField = document.getElementById('second');
 const thirdField = document.getElementById('third');
 
-const MAX_STEPS = 3; 
+const max_step = 3; 
 let currentStep = 1; //Dit is let omdat de current step gaat veranderen en dus nooit constant blijft
+
+
 
 function next () {
     bullets[currentStep  -  1].classList.add('completed');
 	currentStep  +=  1;
 	previousBtn.disabled  =  false;
-	if  (currentStep  ===  MAX_STEPS)  {
-		nextBtn.disabled  =  true;
-		finishBtn.disabled  =  false;
-	}
-    content.innerText  =  `Step Number ${currentStep}`;
-    
-    if (currentStep === 1){
+     if (currentStep === 1){
         firstField.classList.remove("hide");
         secondField.classList.add("hide");
         thirdField.classList.add("hide");
@@ -36,7 +32,11 @@ function next () {
         firstField.classList.add("hide");
         secondField.classList.add("hide");
         thirdField.classList.remove("hide");
+        nextBtn.disabled  =  true;
+		finishBtn.disabled  =  false;
     }
+
+    content.innerText  =  `Step Number ${currentStep}`;
 };
 
 function previous () {
@@ -45,11 +45,7 @@ function previous () {
 	nextBtn.disabled  =  false;
 	finishBtn.disabled  =  true;
 	if  (currentStep  ===  1)  {
-		previousBtn.disabled  =  true;
-	}
-    content.innerText  =  `Step Number ${currentStep}`;
-    
-    if (currentStep === 1){
+        previousBtn.disabled  =  true;
         firstField.classList.remove("hide");
         secondField.classList.add("hide");
         thirdField.classList.add("hide");
@@ -62,32 +58,31 @@ function previous () {
         secondField.classList.add("hide");
         thirdField.classList.remove("hide");
     }
-};
+
+    content.innerText  =  `Step Number ${currentStep}`;
+ };
 
 https://stackoverflow.com/questions/2906582/how-to-create-an-html-button-that-acts-like-a-link
 function over () {
     location.href='profile.html';
 };
 
-
-// function showFirst(){
-//     if (currentStep === 1){
-//         firstField.classList.remove("hide");
-//         secondField.classList.add("hide");
-//         thirdField.classList.add("hide");
-//     } else if (currentStep === 2){
-//         firstField.classList.add("hide");
-//         secondField.classList.remove("hide");
-//         thirdField.classList.add("hide");
-//     } else {
-//         firstField.classList.add("hide");
-//         secondField.classList.add("hide");
-//         thirdField.classList.remove("hide");
-//     }
-// }
-
 nextBtn.addEventListener('click', next)
 previousBtn.addEventListener('click', previous)
 finishBtn.addEventListener('click', over)
+
+//pseudo code
+//Als default staat class hide op secondField en thirdField  (je bent dus bij stap 1)
+//dit gebeurd al in de html
+
+//Wanneer je op stap 2 bent gaan firstField en thirdField op hide, secondField gaat van hide af
+//if currentStep === 2
+//firstField & thirdField .classList add hide
+//secondField .classlist remove hide
+
+//Wanneer je op stap 3 bent gaan firstField en ssecondField op hide, thirdField gaat van hide af
+//if currentStep === 3
+//firstField & secondField .classList add hide
+//thirdField .classlist remove hide
 
 
